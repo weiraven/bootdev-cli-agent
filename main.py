fa25277a5c416a230af8249f226c6d3e3e89d404
Raven Wei <125535543+weiraven@users.noreply.github.com>
2026-03-06 16:15:54 -0500

    improve cli prompt handling and verbose output

--- main.py
+++ main.py
@@ -1,6 +1,48 @@
-def main():
-    print("Hello from bootdev-ai-agent!")
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
+def main():
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    
+    # Argument parsing
+    parser = argparse.ArgumentParser(description="Gemini CLI Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    if api_key is None:
+        raise RuntimeError("GEMINI_API_KEY not found. Make sure it is set in your .env file.")
+
+    client = genai.Client(api_key=api_key)
+
+    messages = [
+        types.Content(
+            role="user", parts=[types.Part(text=args.user_prompt)]
+        )
+    ]
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=messages
+    )
+
+    if response.usage_metadata is None:
+        raise RuntimeError(
+            "No usage metadata returned. The API request may have failed."
+        )
+
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        
+    print("Response:")
+    print(response.text)
 
 if __name__ == "__main__":
     main()
